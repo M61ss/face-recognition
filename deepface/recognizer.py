75e1fe9708c6849558a8b1c6184f00b1bbeff96c
Mattia Massarenti <mattia.mas2003@gmail.com>
2025-12-07 22:26:04 +0100

    Refactor face verification logic in recognizer.py for improved clarity and error handling

--- deepface/recognizer.py
+++ deepface/recognizer.py
@@ -6,14 +6,30 @@ import os
 # Best model: Facenet512
 
 subject : str = 'data/known/mattia.jpg'
-files : list[str] = os.listdir('data/')
+try:
+    print(f"Subject to be detected is '{subject}'")
+    DeepFace.extract_faces(subject)
+except:
+    raise Exception(f"No face detected in '{subject}'")
 
-outputs = []
+files : list[str] = os.listdir('data/batch/')
+
+matches = []
 for file in files:
-    file_path = 'data/' + file
-    if os.path.isfile(file_path):
-        print(f"Verifing {file}")
-        if DeepFace.verify(subject, file_path, model_name='Facenet512'):
-            outputs.append(file_path)
+    file = 'data/batch/' + file
+    if os.path.isfile(file):
+        print(f"Verifing '{file}' ...")
+        try:
+            print(f"Found {len(DeepFace.extract_faces(file))} faces ...")
+            output = DeepFace.verify(subject, file, model_name='DeepFace')
+            print(output)
+            if output['verified']:
+                print("Match!")
+                matches.append(file)
+            else:
+                print("No match")
+            print()
+        except Exception:
+            print("No face detected")
 
-print(f"Subject {subject} found in {len(outputs)} images:\n{outputs}")
+print(f"Subject '{subject}' found in {len(matches)} images:\n{matches}")
